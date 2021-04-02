15e6b5fb4d74bed28e2f8a2ab7f1f4b9b3f6384c
María Belén Ambi González <mabebemabelen310@gmail.com>
2021-04-03 01:46:03 +0200

    FireBase

--- app/src/main/java/com/example/theecobob/SignUp.java
+++ app/src/main/java/com/example/theecobob/SignUp.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class  SignUp extends AppCompatActivity {
 
@@ -14,26 +16,45 @@ public class  SignUp extends AppCompatActivity {
     TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
     Button regBtn, regToLoginBtn;
 
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        //Hooks para todos los elementos XML del activity_sign_up.xml
-        regName = findViewById(R.id.reg_name);
-        regUsername = findViewById(R.id.reg_username);
-        regEmail = findViewById(R.id.reg_email);
-        regPhoneNo = findViewById(R.id.reg_phoneNo);
-        regPassword = findViewById(R.id.reg_password);
-        regBtn = findViewById(R.id.reg_btn);
-        regToLoginBtn = findViewById(R.id.reg_login_btn);
+        //Hooks
+        regName=findViewById(R.id.reg_name);
+        regUsername=findViewById(R.id.reg_username);
+        regEmail=findViewById(R.id.reg_email);
+        regPhoneNo=findViewById(R.id.reg_phoneNo);
+        regPassword=findViewById(R.id.reg_password);
+        regBtn=findViewById(R.id.reg_btn);
+        regToLoginBtn=findViewById(R.id.reg_login_btn);
 
         //Guardar datos en FireBase clickando el botón
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("Users");
+
+                //Get all the values
+                String name = regName.getEditText().getText().toString();
+                String username = regUsername.getEditText().getText().toString();
+                String email = regEmail.getEditText().getText().toString();
+                String phoneNo = regPhoneNo.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+
+
+                UserHelperClass helperClass = new UserHelperClass(name,username,email,phoneNo,password);
+
+                reference.child(phoneNo).setValue(helperClass);
+
             }
         });
+
+
     }
 }
