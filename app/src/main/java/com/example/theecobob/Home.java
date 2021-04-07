9251e60bcba4114b292d691f0396368257443acb
María Belén Ambi González <mabebemabelen310@gmail.com>
2021-04-07 20:36:42 +0200

    Home hecho

--- app/src/main/java/com/example/theecobob/Home.java
+++ app/src/main/java/com/example/theecobob/Home.java
@@ -2,7 +2,10 @@ package com.example.theecobob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Variables
+        ImageButton btnHomePlastico, btnHomePapel, btnHomeVidrio, btnHomeOrgánico, btnHomeResto;
+
+        //Hooks
+        btnHomePlastico = findViewById(R.id.btnPlastico);
+        btnHomePapel = findViewById(R.id.btnPapel);
+        btnHomeVidrio = findViewById(R.id.btnVidrio);
+        btnHomeOrgánico = findViewById(R.id.btnOrganico);
+        btnHomeResto = findViewById(R.id.btnResto);
+
+        //Procedimiento para llamar a la clase
+        btnHomePlastico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentHome = new Intent (Home.this,Amarillo.class);
+                startActivity(intentHome);
+                finish();
+            }
+        });
+
     }
 }
