2b74cb1b643e183aa6a385581ffb7cb08204fccd
María Belén Ambi González <mabebemabelen310@gmail.com>
2021-04-10 18:15:54 +0200

    Recycler view de cards

--- app/src/main/java/com/example/theecobob/Amarillo.java
+++ app/src/main/java/com/example/theecobob/Amarillo.java
@@ -1,14 +1,41 @@
 package com.example.theecobob;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class Amarillo extends AppCompatActivity {
+import com.example.theecobob.HelperClasses.HomeAdapter.FeaturedAdapter;
+import com.example.theecobob.HelperClasses.HomeAdapter.FeaturedHelperClass;
 
+import java.util.ArrayList;
+
+public class Amarillo extends AppCompatActivity {
+    RecyclerView featuredRecycler;
+    RecyclerView.Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amarillo);
+
+
+        //Hooks
+        featuredRecycler=findViewById(R.id.featured_recycler);
+        featuredRecycler();
+    }
+
+    private void featuredRecycler() {
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        ArrayList<FeaturedHelperClass> featuredLocations= new ArrayList<>();
+
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.envasesplasticosamarillos,"Plastico","asfasgasgagsagasgasgs"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.boobie_dibu_asustado,"Holita","asfasgasgagsagasgasgs"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.theecobob,"Holota","asfasgasgagsagasgasgs"));
+
+        adapter=new FeaturedAdapter(featuredLocations);
+        featuredRecycler.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/com/example/theecobob/HelperClasses/HomeAdapter/FeaturedHelperClass.java
@@ -0,0 +1,41 @@
+package com.example.theecobob.HelperClasses.HomeAdapter;
+
+public class FeaturedHelperClass {
+
+    //Variables
+    int image;
+    String tittle, description;
+
+    public FeaturedHelperClass() {
+    }
+
+    public FeaturedHelperClass(int image, String tittle, String description) {
+        this.image = image;
+        this.tittle = tittle;
+        this.description = description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
