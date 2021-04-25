2d2322c817e907376ce2da1592e46b372873a48d
María Belén Ambi González <mabebemabelen310@gmail.com>
2021-04-25 20:54:39 +0200

    Perfiles hechos. Pendiente clases java

---
+++ app/src/main/java/com/example/theecobob/LeaderboardFragment.java
@@ -0,0 +1,36 @@
+package com.example.theecobob;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LeaderboardFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LeaderboardFragment extends Fragment {
+
+
+    public LeaderboardFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+    }
+}
